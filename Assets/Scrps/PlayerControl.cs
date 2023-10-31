5ed95a16717476a27384eb308ae00058348e9acd
Jagglapaff <124145963+Jagglapaff@users.noreply.github.com>
2023-10-31 13:43:50 +0200

    Update PlayerControl.cs

--- Assets/Scrps/PlayerControl.cs
+++ Assets/Scrps/PlayerControl.cs
@@ -4,67 +4,68 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public float moveSpeed = 1.5f;  // Going forward
-
-    public float verticalSpeed = 4.0f;
-
-    public float verticalInput;
-
-    public float horizontalspeed = 4.0f;
-
+    public float baseSpeed = 1.5f;
+    public float acceleration = 1.0f;
+    public float maxSpeed = 5.0f;
     public float horizontalInput;
-
+    public float verticalInput;
     public GameObject projectilePrefab;
-    // Start is called before the first frame update
+
+    // time
+    public float timer = 0.0f;
+    public float changeTimer = 1.0f;
+
+    private float currentSpeed;
+
     void Start()
     {
-
+        currentSpeed = baseSpeed;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Get the current position of the player
-        Vector3 currentPosition = transform.position;
-
-        // Calculate the new position to move to the right
-        Vector3 newPosition = currentPosition + new Vector3(moveSpeed * Time.deltaTime, 0, 0);
-
-        // Update the player's position
-        transform.position = newPosition;
-
-
-        // Lets add away to move up and down
-
-        verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.up * verticalInput * Time.deltaTime * verticalSpeed);
-
-        // To move vertically
         horizontalInput = Input.GetAxis("Horizontal");
-        if (Input.GetKey(KeyCode.LeftArrow))
+        verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(horizontalInput, verticalInput, 0).normalized;
+
+        if (moveDirection != Vector3.zero)
         {
-            Vector3 tempPosition = currentPosition + new Vector3((horizontalInput * 0) * Time.deltaTime, 0, 0);
-
-            // Update the player's position
-            transform.position = tempPosition;
-
+            if (horizontalInput > 0) // Pressing right
+            {
+                currentSpeed += acceleration * Time.deltaTime;
+                currentSpeed = Mathf.Min(currentSpeed, maxSpeed);
+            }
+            else if (horizontalInput < 0) // Pressing left
+            {
+                currentSpeed -= acceleration * Time.deltaTime;
+                currentSpeed = Mathf.Max(currentSpeed, baseSpeed);
+            }
+        }
+        else
+        {
+            // No input, reset to base speed
+            currentSpeed = baseSpeed;
         }
 
-        else if (Input.GetKey(KeyCode.RightArrow))
+        // to keep player in bound
+        timer += changeTimer * Time.deltaTime;
+
+        if (transform.position.x < -10 + timer  )  // Pitää ottaa huomioon liikkuvuus kamera
         {
-
-            Vector3 tempPosition = currentPosition + new Vector3((horizontalInput + moveSpeed) * Time.deltaTime, 0, 0);
-
-            // Update the player's position
-            transform.position = tempPosition;
+            transform.position = new Vector3(-10 + timer, transform.position.y, transform.position.z);
         }
+        //////
+        
 
-        /// Shooting 
+        transform.Translate(moveDirection * currentSpeed * Time.deltaTime);
+
+        // Shooting
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
 
-    }
 
+    }
 }
